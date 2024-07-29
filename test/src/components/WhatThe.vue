78527c29334ad022f25ab8c8104e74a6972385e9
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-07-29 12:18:45 -0400

    a

--- test/src/components/WhatThe.vue
+++ test/src/components/WhatThe.vue
@@ -1,22 +1,33 @@
 <template>
-  <a-scene
-  embedded
-  vr-mode-ui="enabled: false"
-    arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: true"
-    renderer="antialias: true; alpha: false"
-    webxr="optionalFeatures: hit-test;">
+  <div class="scene">
+    <a-scene
+    vr-mode-ui="enabled: false"
+      arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: true"
+      renderer="antialias: true; alpha: false"
+      webxr="optionalFeatures: hit-test;"
+      embedded>
     <a-box 
-    scale="1 1 1"
-    color="#FFF"></a-box>
-  </a-scene>
+      sposition="1 0 0"
+      scale="1 1 1"
+      color="#FFF">
+    </a-box>
+    <a-camera></a-camera>
+    </a-scene>
+  </div>
 </template>
 
 <script setup>
 import 'aframe'
 import '@ar-js-org/ar.js'
+import '@ar-js-org/artoolkit5-js'
+import '@ar-js-org/ar.js-threejs'
+import 'three'
 
 </script>
 
 <style scoped>
-
+.scene {
+  margin-top: 10%;
+  width: 30%;
+}
 </style>
